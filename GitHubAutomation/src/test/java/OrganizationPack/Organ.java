985801099ea78e80c5b3492d2791c0532b25e310
HP <HP@DESKTOP-76U8IES>
2025-06-26 20:37:23 +0530

    Create Github

---
+++ GitHubAutomation/src/test/java/OrganizationPack/Organ.java
@@ -0,0 +1,10 @@
+package OrganizationPack;
+
+public class Organ {
+
+	public static void main(String[] args) {
+		System.out.println("Create Organization");
+
+	}
+
+}
---
+++ GitHubAutomation/src/test/java/contactpack/Contact.java
@@ -0,0 +1,10 @@
+package contactpack;
+
+public class Contact {
+
+	public static void main(String[] args) {
+		System.out.println("Create Contact");
+
+	}
+
+}
