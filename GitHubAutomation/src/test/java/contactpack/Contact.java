bcf50936a84f23cbfa7a7fdcd6a7b46fa4eb839b
kanch <kanch@AMARESHREDDY>
2025-06-27 11:13:35 +0530

    Push

--- GitHubAutomation/src/test/java/contactpack/Contact.java
+++ GitHubAutomation/src/test/java/contactpack/Contact.java
@@ -4,6 +4,9 @@ public class Contact {
 
 	public static void main(String[] args) {
 		System.out.println("Create Contact");
+		
+		System.out.println("Contact Arnold");
+		System.out.println("Contact Divya");
 
 	}
 
